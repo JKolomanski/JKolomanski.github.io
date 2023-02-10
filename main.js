ebc47abad62a96ab3f02f681aaabb920c762e16d
JKolomanski <kubula@onet.eu>
2023-02-10 17:01:24 +0100

    Added project cards to replace the simple links

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ function magicButtonClicked() {
     alert('Hello world!')
     window.open('https://github.com/JKolomanski')
 }
+
+function spaceWarButtonClicked(){
+    window.open('https://github.com/JKolomanski/SpaceWar')
+}
+
+function mapGeneratorButtonClicked(){
+    window.open('https://github.com/JKolomanski/MapGeneretor')
+}
+
+function JKolomanskiGithubIoButtonClicked(){
+    window.open('https://github.com/JKolomanski/JKolomanski.github.io')
+}
