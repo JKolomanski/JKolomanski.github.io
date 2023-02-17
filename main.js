0cf0b6cfcfe74a84935c96cf3ba07185d94f6410
JKolomanski <kubula@onet.eu>
2023-02-17 19:03:23 +0100

    Added fancy animations to navbar buttons

--- main.js
+++ main.js
@@ -20,5 +20,39 @@ function displayNavBar() {
         .then(
           (text) =>
             (document.getElementById("navbarContainer").innerHTML = text)
-        );
+        )
+        .then( () => {
+            const currentUrl = window.location.href;
+            if (currentUrl.includes('About')) {
+                underlineAboutBtn();
+            }
+
+            if (currentUrl.includes('Projects')) {
+                underlineProjectstBtn();
+            }
+        })
+}
+
+
+function aboutButtonClicked() {
+   window.location.replace("../About/");
+}
+
+function projectsButtonClicked() {
+    window.location.replace("../Projects/");
+}
+
+function underlineAboutBtn() {
+    let aboutBtn = document.getElementById('aboutBtn');
+    aboutBtn.classList.add('underline');
+    let ProjectsBtn = document.getElementById('projectsBtn');
+    ProjectsBtn.classList.remove('underline');
+
+}
+
+function underlineProjectstBtn() {
+    let aboutBtn = document.getElementById('aboutBtn');
+    aboutBtn.classList.remove('underline');
+    let ProjectsBtn = document.getElementById('projectsBtn');
+    ProjectsBtn.classList.add('underline');
 }
