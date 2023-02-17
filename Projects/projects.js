367b8df7f8707283256b56baff5e63538faba0a3
JKolomanski <kubula@onet.eu>
2023-02-17 19:11:40 +0100

    Moved all js code concerning the projects page to a seperate file

---
+++ Projects/projects.js
@@ -0,0 +1,11 @@
+function spaceWarButtonClicked(){
+    window.open('https://github.com/JKolomanski/SpaceWar')
+}
+
+function mapGeneratorButtonClicked(){
+    window.open('https://github.com/JKolomanski/MapGeneretor')
+}
+
+function JKolomanskiGithubIoButtonClicked(){
+    window.open('https://github.com/JKolomanski/JKolomanski.github.io')
+}
--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-function spaceWarButtonClicked(){
-    window.open('https://github.com/JKolomanski/SpaceWar')
-}
-
-function mapGeneratorButtonClicked(){
-    window.open('https://github.com/JKolomanski/MapGeneretor')
-}
-
-function JKolomanskiGithubIoButtonClicked(){
-    window.open('https://github.com/JKolomanski/JKolomanski.github.io')
-}
-
 function redirectToAbout() {
     window.location.replace('About/');
 }
