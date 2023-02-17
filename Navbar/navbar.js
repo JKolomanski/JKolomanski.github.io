a98ac7c02861e0283eb77e0ee38b7ed0cc1c3201
JKolomanski <kubula@onet.eu>
2023-02-17 19:08:44 +0100

    Moved all js code concerning the navbar to a seperate file

---
+++ Navbar/navbar.js
@@ -0,0 +1,42 @@
+function displayNavBar() {
+    fetch("../Navbar/navbar.html")
+        .then((response) => response.text())
+        .then(
+          (text) =>
+            (document.getElementById("navbarContainer").innerHTML = text)
+        )
+        .then( () => {
+            const currentUrl = window.location.href;
+            if (currentUrl.includes('About')) {
+                underlineAboutBtn();
+            }
+
+            else if (currentUrl.includes('Projects')) {
+                underlineProjectstBtn();
+            }
+        })
+}
+
+
+function aboutButtonClicked() {
+   window.location.replace("../About/");
+}
+
+function projectsButtonClicked() {
+    window.location.replace("../Projects/");
+}
+
+function underlineAboutBtn() {
+    let aboutBtn = document.getElementById('aboutBtn');
+    aboutBtn.classList.add('underline');
+    let ProjectsBtn = document.getElementById('projectsBtn');
+    ProjectsBtn.classList.remove('underline');
+
+}
+
+function underlineProjectstBtn() {
+    let aboutBtn = document.getElementById('aboutBtn');
+    aboutBtn.classList.remove('underline');
+    let ProjectsBtn = document.getElementById('projectsBtn');
+    ProjectsBtn.classList.add('underline');
+}
