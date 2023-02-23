22356b6acb5c0d2ea44d14d8940899124e681042
JKolomanski <kubula@onet.eu>
2023-02-23 16:53:06 +0100

    Fixed the email button not copying the email to clipboard properly on browsers  other than firefox

--- About/about.js
+++ About/about.js
@@ -2,7 +2,13 @@ function githubButtonClicked(){
     window.open('https://github.com/JKolomanski');
 }
 
-function copyEmailToClipboard() {
-    navigator.clipboard.writeText("kubula@onet.eu");
-    alert("Copied the email adress to clipboard");
+async function copyEmailToClipboard() {
+    const myEmail = 'kubula@onet.eu'
+
+    try {
+        await navigator.clipboard.writeText(myEmail);
+        alert("Copied the email adress to clipboard");
+    } catch (err) {
+        alert("Failed to copy the adress. Please copy it manually: kubula@onet.eu");
+    }
 }
